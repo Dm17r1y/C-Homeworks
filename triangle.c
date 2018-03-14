1292711495285dbde7d3bf8fc273f3f5e3bd53f0
Dm17r1y <dmitriy.kopylov2016@gmail.com>
2018-03-15 00:53:09 +0500

    change triangle

--- triangle.c
+++ triangle.c
@@ -1,4 +1,20 @@
 #include <stdio.h>
+#include <math.h>
+
+#define MAX_SYMBOLS_AFTER_COMMA 10
+#define MEASUREMENT_ERROR 3
+
+// compile with -lm
+
+double round_number(double value, int symbols_after_comma) {
+	if (symbols_after_comma > MAX_SYMBOLS_AFTER_COMMA) {
+		symbols_after_comma = MAX_SYMBOLS_AFTER_COMMA;
+	}
+	double exponent = pow(10, symbols_after_comma);
+	value *= exponent;
+	value = round(value);
+	return value / exponent;
+}
 
 int main(void) 
 {
@@ -6,9 +22,9 @@ int main(void)
 	double second;
 	double third;
 	scanf("%lf %lf %lf", &first, &second, &third);
-	if ((first*first + second*second == third*third) || 
-		(first*first + third*third == second*second) ||
-		(second*second + third*third == first*first))
+	if ((round_number(first*first + second*second, MEASUREMENT_ERROR) == round_number(third*third, MEASUREMENT_ERROR)) || 
+	    (round_number(first*first + third*third, MEASUREMENT_ERROR) == round_number(second*second, MEASUREMENT_ERROR)) ||
+	    (round_number(second*second + third*third, MEASUREMENT_ERROR) == round_number(first*first, MEASUREMENT_ERROR)))
 	{
 		puts("True");
 	}
