e8c62e86d6e6b5daa875d93f90575a1fec7d1963
Dm17r1y <dmitriy.kopylov2016@gmail.com>
2018-03-15 15:19:25 +0500

    change triangle

--- triangle.c
+++ triangle.c
@@ -1,32 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include <math.h>
 
-#define MAX_SYMBOLS_AFTER_COMMA 10
-#define MEASUREMENT_ERROR 3
+#define EPSILON 0.001
 
-// compile with -lm
-
-double round_number(double value, int symbols_after_comma) {
-	if (symbols_after_comma > MAX_SYMBOLS_AFTER_COMMA) {
-		symbols_after_comma = MAX_SYMBOLS_AFTER_COMMA;
-	}
-	double exponent = pow(10, symbols_after_comma);
-	value *= exponent;
-	value = round(value);
-	return value / exponent;
-}
+bool is_triangle(int, int, int);
+bool is_equial(double, double, double);
 
 int main(void)  {
 	double first;
 	double second;
 	double third;
 	scanf("%lf %lf %lf", &first, &second, &third);
-	if ((round_number(first*first + second*second, MEASUREMENT_ERROR) == round_number(third*third, MEASUREMENT_ERROR)) || 
-	    (round_number(first*first + third*third, MEASUREMENT_ERROR) == round_number(second*second, MEASUREMENT_ERROR)) ||
-	    (round_number(second*second + third*third, MEASUREMENT_ERROR) == round_number(first*first, MEASUREMENT_ERROR))) {
+	if (!is_triangle(first, second, third)) {
+		puts("Not a triangle");
+		exit(1);
+	}
+	if (is_equial(first*first + second*second, third*third, EPSILON)) {
 		puts("True");
 	}
 	else {
 		puts("False");
 	}
+}
+
+bool is_triangle(int a, int b, int c) {
+	return a + b > c && a + c > b && b + c > a;
+}
+
+bool is_equial(double first, double second, double epsilon) {
+	return fabs(first - second) < epsilon;
 }
